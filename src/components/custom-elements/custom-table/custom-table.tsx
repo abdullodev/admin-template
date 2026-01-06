7776e0da27f2ae9fba88add8066ce1b558b89945
abdullo.ergashxujaev <abdullo.ergashxujayev@fido.uz>
2026-01-06 13:57:30 +0500

    some changes

--- src/components/custom-elements/custom-table/custom-table.tsx
+++ src/components/custom-elements/custom-table/custom-table.tsx
@@ -1,9 +1,9 @@
 function CustomTable() {
-  return (
-    <div className="flex flex-col min-w-[800px] overflow-auto">
-      <h2>Table</h2>
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-w-[800px] overflow-auto">
+            <h2>Table</h2>
+        </div>
+    );
 }
 
 export default CustomTable;
